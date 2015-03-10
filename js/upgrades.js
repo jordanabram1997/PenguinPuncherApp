6c300eea4194a97c0483b57b11338f0d0de64fd3
jordan abram <jordan16abram@gmail.com>
2015-03-10 13:39:21 +0000

    Changed Reference of `dps` & `dpc` on `upgrades.js` Changed all references to `dpc` & `dps` Changed the Output name to `.js-updates-output` Made Changes as requested by @aaronlayton Re-structed the Game HUD fixes #26

--- js/upgrades.js
+++ js/upgrades.js
@@ -2,120 +2,120 @@ var data = {
 	upgrades: [
 		{
 			title: "More Punches",
-			dpc: 1,
-			dps: 0,
+			ppc: 1,
+			pps: 0,
 			price:50,
 			image:"img/morepunches.png",
 			multiplier:1.1
 		},
 		{
 			title: "Ice Breaker",
-			dpc: 0,
-			dps: 1,
+			ppc: 0,
+			pps: 1,
 			price:250,
 			image:"img/icecube.png",
 			multiplier:1.1
 		},
 		{
 			title: "Sharp Slaps",
-			dpc: 0,
-			dps: 4,
+			ppc: 0,
+			pps: 4,
 			price:1000,
 			image:"img/sharpslaps.png",
 			multiplier:1.5
 		},
 		{
 			title: "Iceberg",
-			dpc: 0,
-			dps: 15,
+			ppc: 0,
+			pps: 15,
 			price:5000,
 			image:"img/iceberg.png",
 			multiplier:1.5
 		},
 		{
 			title: "Super Punch",
-			dpc: 0,
-			dps: 60,
+			ppc: 0,
+			pps: 60,
 			price:25000,
 			image:"img/superpunch.png",
 			multiplier:1.5
 		},
 		{
 			title: "Melting Ice",
-			dpc: 0,
-			dps: 200,
+			ppc: 0,
+			pps: 200,
 			price:100000,
 			image:"img/meltingice.png",
 			multiplier:1.5
 		},
 		{
 			title: "Ice Sword",
-			dpc: 0,
-			dps: 1000,
+			ppc: 0,
+			pps: 1000,
 			price:750000,
 			image:"img/icesword.png",
 			multiplier:1.5
 		},
 		{
 			title: "Emerald Pickaxe",
-			dpc: 0,
-			dps: 3500,
+			ppc: 0,
+			pps: 3500,
 			price:800000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Friend",
-			dps: 0,
-			dpc: 10,
+			pps: 0,
+			ppc: 10,
 			price:50000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "TNT",
-			dpc: 0,
-			dps: 10000,
+			ppc: 0,
+			pps: 10000,
 			price:175500000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Silverfish",
-			dpc: 0,
-			dps: 50000,
+			ppc: 0,
+			pps: 50000,
 			price:100000000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Enderman",
-			dpc: 0,
-			dps: 3000000,
+			ppc: 0,
+			pps: 3000000,
 			price:1000000000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Ghast",
-			dpc: 0,
-			dps: 1750000,
+			ppc: 0,
+			pps: 1750000,
 			price:7500000000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Wither",
-			dpc: 0,
-			dps: 7500000,
+			ppc: 0,
+			pps: 7500000,
 			price:50000000000,
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
 		},
 		{
 			title: "Coming Soon",
-			dpc: "?",
-			dps: "?",
+			ppc: "?",
+			pps: "?",
 			price:"?",
 			image:"img/diamond-pickaxe.png",
 			multiplier:1.5
